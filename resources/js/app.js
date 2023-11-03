e34abf123b3ecc6776d3dade16d96891f4f7a58e
kole <misterija@gmail.com>
2023-11-03 16:06:01 +0100

    create app, add bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+// import BootstrapVue from 'bootstrap-vue' //Importing
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import "bootstrap/dist/css/bootstrap.css";
+const app = createApp(App);
+// app.use(BootstrapVue) 
+app.mount('#app')
+import "bootstrap/dist/js/bootstrap.js";
