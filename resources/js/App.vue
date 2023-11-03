3534b45621cede38ffedc5760b321676c467236d
kole <misterija@gmail.com>
2023-11-03 19:20:12 +0100

    Test vuex store with counter

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>VUE 3</h1>
-</template>
+    <div>
+      <p>Count: {{ count }}</p>
+      <button @click="incrementCount">Increment</button>
+      <button @click="decrementCount">Decrement</button>
+    </div>
+  </template>
+  
+  <script>
+  import { mapState, mapActions } from "vuex";
+  
+  export default {
+    computed: {
+      ...mapState("counter", ["count"]), // "counter" is the module name, "count" is the state property
+    },
+    methods: {
+      ...mapActions("counter", ["incrementCount", "decrementCount"]), // Actions from the "counter" module
+    },
+  };
+  </script>
+  
+  
+  
+  
+  
