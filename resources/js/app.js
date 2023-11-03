5f635defd5945487df0dedcd97550e00a8053376
kole <misterija@gmail.com>
2023-11-03 19:19:46 +0100

    include store and router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,19 @@
 import './bootstrap';
-// import BootstrapVue from 'bootstrap-vue' //Importing
-
 import { createApp } from 'vue';
+
+import Vuex from 'vuex';
+import counter from './store/counter/index';
+
+import router from './routes/index'
 import App from './App.vue';
 import "bootstrap/dist/css/bootstrap.css";
 const app = createApp(App);
-// app.use(BootstrapVue) 
+app.use(router)
+const store = new Vuex.Store({
+    modules: {
+        counter
+    }
+})
+app.use(store) 
 app.mount('#app')
 import "bootstrap/dist/js/bootstrap.js";
