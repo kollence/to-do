3ae7fb3382776ad0f85c4395d1b9730076c01459
kole <misterija@gmail.com>
2023-11-04 18:43:18 +0100

    clear links from layouts & moved to nav component

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Auth layout</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6 border">
+                <div class="alert" role="alert">
+                    {{ message }}
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            message: "Authenticated Layout"
+        }
+    },
+
+}
+</script>
--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>Guest Layout</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6 border">
+                <div class="alert" role="alert">
+                    {{ message }}
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            message: "Guest Layout"
+        }
+    }
+}
+</script>
