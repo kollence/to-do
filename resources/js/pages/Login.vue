c2b52989eb9bb17bbb5eccb5c82e15f15cb94139
kole <misterija@gmail.com>
2023-11-04 18:44:04 +0100

    Login & Register with vuex store

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,3 +1,85 @@
 <template>
-    <h1>Login</h1>
-</template>
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="card">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+              <form @submit.prevent="loginUser">
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    v-model="formData.email"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    v-model="formData.password"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        formData: {
+          email: '',
+          password: '',
+        },
+      };
+    },
+    methods: {
+      async loginUser() {
+        // Call the Vuex action for login
+        await this.$store.dispatch('auth/login', this.formData).then(path => {
+            this.$router.push(path)
+        });
+      },
+    },
+  };
+  </script>
+  
+
+<!-- <script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            form: {
+            email: '',
+            password: '',
+            },
+            errors: { },
+        }
+    },
+    methods: {
+        async handleLogin() {
+            try {
+                // await this.$store.dispatch('login', this.form)
+                await axios.get('/sanctum/csrf-cookie');
+                await axios.post('/api/login', this.form)
+                this.$router.push('todo') 
+                alert('Login successful!')
+            } catch (error) {
+                this.errors = error.response.data.errors
+            }
+        }
+    }
+}
+</script> -->
--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -1,3 +1,77 @@
 <template>
-    <h1>Register</h1>
-</template>
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="card">
+            <div class="card-header">Register</div>
+            <div class="card-body">
+              <form @submit.prevent="registerUser">
+                <div class="form-group">
+                  <label for="name">Name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    v-model="formData.name"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    v-model="formData.email"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    v-model="formData.password"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password_confirmation">Confirm Password</label>
+                  <input
+                    type="password"
+                    id="password_confirmation"
+                    v-model="formData.password_confirmation"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        formData: {
+          name: '',
+          email: '',
+          password: '',
+          password_confirmation: '',
+        },
+      };
+    },
+    methods: {
+      async registerUser() {
+        // Call the Vuex action for registration
+        this.$store.dispatch('auth/register', this.formData).then(path => {
+            this.$router.push(path)
+        });
+      },
+    },
+  };
+  </script>
