b7cb3b21af7b336a5f1c09dd32360691d32a6890
kole <misterija@gmail.com>
2023-11-04 18:41:07 +0100

    swal & store for auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,26 @@
 import './bootstrap';
+import "bootstrap/dist/css/bootstrap.css";
+
+import VueSweetalert2 from 'vue-sweetalert2';
 import { createApp } from 'vue';
 
 import Vuex from 'vuex';
-import counter from './store/counter/index';
+import counter from './store/counter';
+import auth from './store/auth'
 
-import router from './routes/index'
+import router from './routes'
 import App from './App.vue';
-import "bootstrap/dist/css/bootstrap.css";
+
+
 const app = createApp(App);
 app.use(router)
 const store = new Vuex.Store({
     modules: {
-        counter
+        counter,
+        auth
     }
 })
+app.use(VueSweetalert2);
 app.use(store) 
 app.mount('#app')
 import "bootstrap/dist/js/bootstrap.js";
