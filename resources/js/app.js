eb039dcf3f71bff85a7bc879ce5223ad4efd4262
kole <misterija@gmail.com>
2023-11-05 21:33:01 +0100

    swal css & todo vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,27 @@
 import './bootstrap';
 import "bootstrap/dist/css/bootstrap.css";
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import { createApp } from 'vue';
 
 import Vuex from 'vuex';
-import counter from './store/counter';
 import auth from './store/auth'
+import todo from './store/todo'
 
 import router from './routes'
 import App from './App.vue';
 
 
 const app = createApp(App);
-app.use(router)
+
 const store = new Vuex.Store({
     modules: {
-        counter,
-        auth
+        auth,
+        todo
     }
 })
+app.use(router)
 app.use(VueSweetalert2);
 app.use(store) 
 app.mount('#app')
