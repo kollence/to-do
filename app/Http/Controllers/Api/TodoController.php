5ffbfc1dc0807d83dde683abbcedfbb37e867534
kole <misterija@gmail.com>
2023-11-05 14:25:37 +0100

    TodoController with index

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TodoResource;
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
@@ -14,7 +17,18 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+
+        if($user->isAdmin()){
+
+            $todos = Todo::all();
+
+            return TodoResource::collection($todos);
+        }
+        // Get all todos created by the authenticated user
+        $todos = $user->todos;
+
+        return TodoResource::collection($todos);
     }
 
     /**
