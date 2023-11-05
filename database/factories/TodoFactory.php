fcf46bebf42251b7663a2f834d6e0859b9d3611f
kole <misterija@gmail.com>
2023-11-05 14:30:32 +0100

    factory, seeder & migration for todo

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'completed' => fake()->boolean(false),
+            'user_id' => fn () => \App\Models\User::factory()->create()->id,
+        ];
+    }
+}
