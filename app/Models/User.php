94e82ec7cf17634d6b387754292f4a5f85f06048
kole <misterija@gmail.com>
2023-11-05 14:28:33 +0100

    User model with relationship and isAdmin simple rule

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function todos(){
+        return $this->hasMany(Todo::class);
+    }
+
+    public function isAdmin()
+    {
+        return $this->email === 'admin@mail.com';
+    }
 }
