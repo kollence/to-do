c56b464073b4f97524e86d1d86d211714fd43134
kole <misterija@gmail.com>
2023-11-05 14:30:53 +0100

    fix plural route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthApiController;
+use App\Http\Controllers\Api\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,5 +30,5 @@ Route::group(['middleware'=>'auth:sanctum'], function () {
         ]];
     });
 
-    Route::apiResource('todo', TodoController::class);
+    Route::apiResource('todos', TodoController::class);
 });
