9d70cce28dc220a3fd451f04d384afae7f5640aa
kole <misterija@gmail.com>
2023-11-07 13:43:16 +0100

    check if user is auth on backend GET user

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,31 @@ import todo from './store/todo'
 import router from './routes'
 import App from './App.vue';
 
+//Check if user is authenticated on backend
+router.beforeEach(async (to, from, next) => {
+    const protectedRouteNames = ['todos', 'todo.create', 'todo.edit', 'todo.show'];
+    if (protectedRouteNames.includes(to.name)) {
+      // Check the user's authentication status by making a request to /api/user
+      try {
+        // Make an authenticated GET request to your backend
+        const response = await axios.get('/api/user');
+        if (response.statusText == "OK") {
+          // User is authenticated, continue navigating
+          next();
+        } else {
+          // User is not authenticated, redirect to a login or unauthorized route
+          next({ name: 'login' });
+        }
+      } catch (error) {
+        // Handle network errors or any other issues
+        console.error('Authentication check failed:', error);
+        next({ name: 'error' });
+      }
+    } else {
+      // For other routes, no authentication check is needed, so continue navigating
+      next();
+    }
+  });
 
 const app = createApp(App);
 
